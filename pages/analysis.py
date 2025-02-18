fa6df5a502ed6fc77db3e074777bb19a6543832a
Swayam Jaiswal <102657873+swayamjaiswal7@users.noreply.github.com>
2025-02-18 13:48:32 +0530

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+
 st.write("Here's a overview of the Automobile datasets")
 df=pd.read_csv('Automobile.csv')
 st.table(df)
